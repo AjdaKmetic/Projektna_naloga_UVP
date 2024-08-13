0ec6d8d82affca2b58c6ef7ac0a86db00838f303
Ajda Kmetič <kmetic.ajda@gmail.com>
2024-08-13 21:19:09 +0200

    Brisanje komentarjev.

--- zbiranje_podatkov.py
+++ zbiranje_podatkov.py
@@ -7,17 +7,10 @@ def main():
     igralci_html = 'igralci.html'
     igralci_csv = 'igralci.csv'
 
-    # Najprej v lokalno datoteko shranimo glavno stran
-    # pot1 = os.path.join(igralci_mapa, igralci_html)
-    # if redownload or not os.path.exists(pot1):
     shrani_stran(link, igralci_mapa, igralci_html)
 
-    # Podatke preberemo v lepšo obliko (seznam slovarjev)
     seznam_igralcev = igralci_iz_datoteke(igralci_mapa, igralci_html)
 
-    # Podatke shranimo v csv datoteko
-    # pot2 = os.path.join(igralci_mapa, igralci_csv)
-    # if reparse or not os.path.exists(pot2):
     zapisi_igralce_v_csv(seznam_igralcev, igralci_mapa, igralci_csv)
 
 if __name__ == '__main__':
